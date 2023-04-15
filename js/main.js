7eaeea82f91bcbb4afe8505ae4efb4e8c8a70d7f
Mudassir Mehdi <mudssair.madhi5@gmail.com>
2023-04-15 23:51:00 +0500

    Done most of the work

--- js/main.js
+++ js/main.js
@@ -1,50 +1,13 @@
-(function() {
-
-  
-
-    var parent = document.querySelector(".range-slider");
-    if(!parent) return;
-  
-    var
-      rangeS = parent.querySelectorAll("input[type=range]"),
-      numberS = parent.querySelectorAll("input[type=number]");
-  
-    rangeS.forEach(function(el) {
-      el.oninput = function() {
-        var slide1 = parseFloat(rangeS[0].value),
-              slide2 = parseFloat(rangeS[1].value);
-  
-        if (slide1 > slide2) {
-                  [slide1, slide2] = [slide2, slide1];
-          // var tmp = slide2;
-          // slide2 = slide1;
-          // slide1 = tmp;
-        }
-  
-        numberS[0].value = slide1;
-        numberS[1].value = slide2;
-      }
-    });
-  
-    numberS.forEach(function(el) {
-      el.oninput = function() {
-              var number1 = parseFloat(numberS[0].value),
-                      number2 = parseFloat(numberS[1].value);
-              
-        if (number1 > number2) {
-          var tmp = number1;
-          numberS[0].value = number2;
-          numberS[1].value = tmp;
-        }
-  
-        rangeS[0].value = number1;
-        rangeS[1].value = number2;
-  
-      }
-    });
-
-    
-  
-  })();
-
-  
+$(function() {
+  $( ".slider-range" ).slider({
+  range: true,
+  min: 0,
+  max: 15340000, 
+  values: [ 0, 15340000 ],
+  slide: function( event, ui ) {
+      $( ".amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+  }
+  });
+  $( ".amount" ).val( "$" + $( ".slider-range" ).slider( "values", 0 ) +
+  " - $" + $( ".slider-range" ).slider( "values", 1 ) );
+});
